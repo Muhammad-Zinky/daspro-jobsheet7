3ac341c78f859e7fdfbbfbb51d1af2becb5deadb
mohammad zinky <mohammadzinky@gmail.com>
2024-10-15 16:26:21 +0700

    modifikasi

--- SiakadWhile10.java
+++ SiakadWhile10.java
@@ -5,11 +5,11 @@ public class SiakadWhile10 {
         Scanner sc = new Scanner(System.in);
         int nilai, jml, i = 0;
         
-        System.out.print("Masukkan jumlah mahasiswa: ");
+        System.out.println("Masukkan jumlah mahasiswa: ");
         jml = sc.nextInt();
 
         while (i < jml) {
-            System.out.print("Masukkan nilai mahasiswa ke- " + (i + 1) + ": ");
+            System.out.println("Masukkan nilai mahasiswa ke- " + (i + 1) + ": ");
             nilai = sc.nextInt();
 
             if (nilai < 0 || nilai > 100) {
@@ -19,6 +19,7 @@ public class SiakadWhile10 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiwa ke-" + (i + 1) + " adalah A");
+                System.out.println(" Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
